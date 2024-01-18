cf4bb8dbc5c418e03ad2c2c12a93a83747d1b371
Falguni axar babariya <145891589+fbabariya@users.noreply.github.com>
2024-01-18 14:36:43 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,32 @@
+print("enter first digit :")
+a=int(input())
+print("enter second digit :")
+b=int(input())
+print("enter desired operation to perform :")
+enter_symbol=input()
+if enter_symbol=="+":
+    result=a+b
+elif enter_symbol=="-":
+    result=a-b
+elif enter_symbol=="*":
+    result=a*b
+elif enter_symbol=="/":
+    result=a/b
+elif enter_symbol=="%":
+    result=a%b
+
+print(result)
+b=int(input())
+enter_symbol=input()
+if enter_symbol=="+":
+    result=a+b
+elif enter_symbol=="-":
+    result=a-b
+elif enter_symbol=="*":
+    result=a*b
+elif enter_symbol=="/":
+    result=a/b
+elif enter_symbol=="%":
+    result=a%b
+
+print(result)
